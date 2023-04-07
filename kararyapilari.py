ca8eb56b8e9743f1224096c685009419583067de
AyseAydinn <129997992+AyseAydinn@users.noreply.github.com>
2023-04-07 11:35:02 +0300

    .

--- kararyapilari.py
+++ kararyapilari.py
@@ -20,3 +20,35 @@ else:
     print("Kaldınız")
 
 
+print("-----------------------------------------------------")
+
+
+studentCount = 44
+if studentCount > 20:
+    print("Öğrenciler ders için hazır")
+
+print("-----------------------------------------------------")
+
+vize = int(input("Vize notunuzu giriniz: "))
+final = int(input("Final notunuzu giriniz: "))
+ortalama = (vize * 0.4) + (final * 0.6) 
+print("Ortalama: ", ortalama, "\nVize: ", vize,  "\nFinal: ", final)
+
+# eğer final 40'dan küçükse kullanıcı kaldı
+# eğer ortalama 50'den küçükse kullanıcı kaldı
+# eğer vize finalin 2 katı ise kullanıcı kaldı
+# bunun dışındaki tüm durumlarda kullanıcı geçti yazdırmak istiyoruz.
+
+#condition-logic
+# or and
+# (true or false) => true => sol ve sağındaki koşullardan en az birisinin true olmasını istiyor 
+# (true and false) => false => sol ve sağındaki koşulların ikisinin de kesinlikle true olmasını ister
+
+if (final < 40) or (ortalama < 50) or (vize == final *2):
+    print("Kaldı")
+# elif ortalama < 50:
+#     print("Kaldı")
+# elif vize == final * 2:
+#     print("Kaldı")
+else:
+    print("Geçti")
